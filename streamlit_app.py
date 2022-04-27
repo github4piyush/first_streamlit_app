e21cbdc8f36ac3daf68b1c420c36afbd4c13025b
Piyush <snowflakepiyush@gmail.com>
2022-04-26 23:43:58 -0400

    Update streamlit_app.py
    fixed typo in S3 link

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ streamlit.text('🥑 🥪 Avocado Toast')
 streamlit.header('🍌 🍓 Build Your Own Fruit Smoothie 🥝 🍇 🍹')
 
 import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us.west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)

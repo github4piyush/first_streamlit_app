e53ff79b0cb1a7a9b3579e3851f2173fd64eaddb
Piyush <snowflakepiyush@gmail.com>
2022-04-26 23:42:07 -0400

    Update streamlit_app.py
    imported file from aws S3 and pulled into python pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,7 @@ streamlit.text('🥚 🐥 Hard-Bioled Free-Range Egg')
 streamlit.text('🥑 🥪 Avocado Toast')
 
 streamlit.header('🍌 🍓 Build Your Own Fruit Smoothie 🥝 🍇 🍹')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us.west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)

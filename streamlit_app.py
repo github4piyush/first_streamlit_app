34857fc658efb6c24a1088e80a4aaacafdc42421
Piyush <snowflakepiyush@gmail.com>
2022-04-26 23:36:41 -0400

    Update streamlit_app.py
    added BYO smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 🐥 Hard-Bioled Free-Range Egg')
 streamlit.text('🥑 🥪 Avocado Toast')
+
+streamlit.header('🍌 🍓 Build Your Own Fruit Smoothie 🥝 🍇 🍹')

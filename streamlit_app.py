d727a3f0d66eaeea1da7b63d7b0d99ebd106ca6b
Piyush <snowflakepiyush@gmail.com>
2022-05-02 22:39:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from urllib.error import URLError
 
 streamlit.title('My Mom\'s New Healthy Diner🇸')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 🐥 Hard-Bioled Free-Range Egg')
